4d9daab12bc577e18cafdcc89da159bfb472493c
Karelin Konstantin <kkarelin@croc.ru>
2023-03-09 14:38:34 +0300

    #1 added server config and first component

---
+++ src/FiltersDashlet/FiltersDashlet.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FiltersDashlet = () => {
+  return (
+    <div style={{fontSize: "50px", color: "red"}}>
+      Filters Dashlet
+    </div>
+  );
+};
+
+export default FiltersDashlet;
